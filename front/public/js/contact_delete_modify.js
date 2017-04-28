aff4e714c055de5d1d02f224d932dbab5714840f
ookooer <fly_in_wind@qq.com>
2017-04-28 08:24:15 -0700

    for tabs

--- front/public/js/contact_delete_modify.js
+++ front/public/js/contact_delete_modify.js
@@ -1,66 +1,74 @@
-var demo = new Vue({
-	el: '#app',
-	data: {		
-		contacts: [],
-		apiUrl: 'http://localhost:8089/contacts'
-	},
-	ready: function() {
-		this.getCustomers();				
-	}, 
-	methods: {
-		getCustomers: function() {
-			var vm = this;
+/**
+ *  v1.0.0
+ *  ookooer
+ * 
+ *  执行contact数据的删除/修改工作
+ */
 
-            var http_request = new XMLHttpRequest();            
-			http_request.onreadystatechange = function(){
-			   if (http_request.readyState == 4  || http_request.readyState == 200){
-					console.log(http_request.responseText)
-					
-					var jsonObj = JSON.parse(http_request.responseText);
-					vm.contacts = jsonObj;					
-               }
+
+/**
+* Vue实例
+* 从后端加载数据
+*/
+var demo = new Vue({ 
+        el: '#app', 
+        data: { 
+            contacts: [], 
+            apiUrl: 'http://localhost:8089/contacts' 
+        }, 
+        
+        ready: function() { 
+            this.getCustomers();                
+        }, 
+            
+        methods: { 
+            getCustomers: function() { 
+                var vm = this;
+                var http_request = new XMLHttpRequest();            
+                http_request.onreadystatechange = function()    { 
+                    if (http_request.readyState == 4  || http_request.readyState == 200){ 
+                            console.log(http_request.responseText)
+
+                            var jsonObj = JSON.parse(http_request.responseText); 
+                            vm.contacts = jsonObj;                  
+                    }
+                } 
+
+                http_request.open("GET", this.apiUrl, true); 
+                http_request.send(null);
             } 
-			
-            http_request.open("GET", this.apiUrl, true);
-            http_request.send(null);			
-		}
-	}
-})
+        }
+    })
 
+/**
+* 删除数据，然后重新加载本页面
+*
+* @param {int} en
+*/
+function deleteEntry(en)    { 
+    console.log("delete " + en);
 
-function deleteEntry(en)	{
-	console.log("delete " + en);
+    var http_request = new XMLHttpRequest();            
+    http_request.onreadystatechange = function(){ 
+        if (http_request.readyState == 4  
+            || http_request.readyState == 200)  { 
+            console.log(this.responseText);
+            
+            window.location.href=window.location.href; 
+            window.location.reload; 
+        } 
+    } 
 
-	var http_request = new XMLHttpRequest();            
-	http_request.onreadystatechange = function(){
-	   if (http_request.readyState == 4  || http_request.readyState == 200){
-			console.log(this.responseText);
-			
-			window.location.href=window.location.href; 
-			window.location.reload; 
-	   }
-	} 
-	
-	http_request.open("DELETE", "http://localhost:8089/contacts/" + en, true);
-	http_request.send(null);	
+    http_request.open("DELETE", "http://localhost:8089/contacts/" + en, true); 
+    http_request.send(null);    
 }
 
-function modifyEntry(en)	{
-	console.log("modify " + en);
-	window.location.href='./addcontact?id=' + en;
-	
-	/*
-	var http_request = new XMLHttpRequest();            
-	http_request.onreadystatechange = function(){
-	   if (http_request.readyState == 4  || http_request.readyState == 200){
-			console.log(this.responseText);
-			
-			window.location.href=window.location.href; 
-			window.location.reload; 
-	   }
-	} 
-	
-	http_request.open("PUT", "http://localhost:8089/contacts/:" + en, true);
-	http_request.send(null);	 
-	*/
+/**
+* 跳转到其它页面修改数据
+*
+* @param {int} en
+*/
+function modifyEntry(en)    { 
+    console.log("modify " + en); 
+    window.location.href='./addcontact?id=' + en;
 }
